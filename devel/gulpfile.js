9c4fc3373148821419e228c948879b00de78aa69
lucyia <lucia.koc@gmail.com>
2016-02-09 23:57:52 +0100

    Basic setup with gulp
    Added gulpfile, index, less

---
+++ devel/gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var sync = require('browser-sync').create();
+var concat = require('gulp-concat');
+var minify = require('gulp-minify');
+var less = require('gulp-less');
+var cssnano = require('gulp-cssnano');
+
+gulp.task('default', ['watch']);
+
+gulp.task('watch', ['sync', 'less', 'scripts'], function(){
+	gulp.watch('./*/*.less', ['less']);
+	gulp.watch('./*/*.js', ['scripts']);
+	gulp.watch('../dist/index.html', sync.reload);	
+});
+
+gulp.task('sync', function(){
+	sync.init({
+		server: {
+			baseDir: '../dist/'
+		},
+	})
+});
+
+gulp.task('less', function () {
+	return gulp.src('./less/style.less')
+		.pipe(less())
+		.pipe(cssnano())
+		.pipe(gulp.dest('../dist'))
+		.pipe(sync.reload({
+			stream: true
+		}))
+});
+
+gulp.task('scripts', function(){
+	return gulp.src([
+			'./node_modules/jquery/dist/jquery.js',			
+			'./node_modules/bootstrap/dist/js/bootstrap.js',
+			'./js/*.js'
+			])
+		.pipe(concat('scripts.js'))
+		.pipe(minify())
+		.pipe(gulp.dest('../dist'))
+});
