315bed58bcfe5859a7e1156278a767d6b16cc094
lucyia <lucia.koc@gmail.com>
2016-02-11 18:26:00 +0100

    Pattern generation updated
    Bug with appending more elements needed fixed

--- dist/scripts.js
+++ dist/scripts.js
@@ -21777,40 +21777,52 @@ if (typeof jQuery === 'undefined') {
 		var u = 25;
 
 		// create elements where i is one diagonal line and j is one element in line
-		// number of elements is not precise so there is save overlap without missing border elements
-		for (var i=0; i<canvasHeight/(u*4)*4; i++) {
-			for (var j=0; j<canvasWidth/(u*4)*4; j++) {
+		// generating more elements and then checking whether it overlaps with canvas
+		// so there is save overlap without missing border elements
+		for (var i=0; i<Math.max(canvasWidth, canvasHeight)/(u*4)*5; i++) {
+			for (var j=0; j<Math.max(canvasHeight, canvasWidth)/(u*4)*5; j++) {
 
 				// set strating point which is outside of canvas
-				var sPointX = -canvasWidth*2 + 4*u*i + 5*u*j;
-				var sPointY = (1.75)*u + 3.5*u*i - 1.75*u*j;
+				var sPointX = -canvasWidth*5 + 4*u*i + 5*u*j;
+				var sPointY = (1.75)*u + 3.5*u*i - 1.75*u*j;				
 
-				//create the element
-				svg.append('path')
-					.attr('d', function(){
-						return	 ' M ' + (sPointX)		 +' '+ (sPointY)
-								+' L ' + (sPointX + 1*u) +' '+ (sPointY - 1.75*u) 
-								+' L ' + (sPointX + 5*u) +' '+ (sPointY - 1.75*u) 
-								+' L ' + (sPointX + 6*u) +' '+ (sPointY) 
-								+' L ' + (sPointX + 4*u) +' '+ (sPointY + 3.5*u)
-								+' L ' + (sPointX + 2*u) +' '+ (sPointY + 3.5*u)
-								+' L ' + (sPointX)       +' '+ (sPointY)
-								+' z'
-					})
-					.attr('fill', 'black')
-					.style('opacity', 0.8)
-					.on('mouseover', function(){
-						d3.select(this)					
-							.transition()
-							.style('opacity', 0)
-							.duration(500);
-					})
-					.on('mouseout', function(){
-						d3.select(this)					
-							.transition()
-							.style('opacity', 0.8)
-							.duration(500);
-					});
+				// if the element ovelaps canvas, then append it, otherwise ignore
+				if (elementInCanvas(sPointX, sPointY)) {
+					svg.append('path')
+						.attr('d', function(){
+							return	 ' M ' + (sPointX)		 +' '+ (sPointY)
+									+' L ' + (sPointX + 1*u) +' '+ (sPointY - 1.75*u) 
+									+' L ' + (sPointX + 5*u) +' '+ (sPointY - 1.75*u) 
+									+' L ' + (sPointX + 6*u) +' '+ (sPointY) 
+									+' L ' + (sPointX + 4*u) +' '+ (sPointY + 3.5*u)
+									+' L ' + (sPointX + 2*u) +' '+ (sPointY + 3.5*u)
+									+' L ' + (sPointX)       +' '+ (sPointY)
+									+' z'
+						})
+						.attr('fill', 'black')
+						.style('opacity', 0.8)
+						.on('mouseover', function(){
+							d3.select(this)					
+								.transition()
+								.style('opacity', 0)
+								.duration(500);
+						})
+						.on('mouseout', function(){
+							d3.select(this)					
+								.transition()
+								.style('opacity', 0.8)
+								.duration(500);
+						});
+				}
+			}
+		}
+
+		// element is overlapping canvas if at least one of its point is in canvas
+		function elementInCanvas(x, y) {			
+			return (pointInCanvas(x, y) || pointInCanvas(x+1*u, y-1.75*u) || pointInCanvas(x+5*u, y-1.75*u) || pointInCanvas(x+6*u, y) || pointInCanvas(x+4*u, y+3.5*u) || pointInCanvas(x+2*u, y+3.5*u));
+
+			function pointInCanvas(x, y){				
+				return (x>0 && x<canvasWidth && y>0 && y<canvasHeight);
 			}
 		}		
 
