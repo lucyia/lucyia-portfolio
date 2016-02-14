8e2da30ff52c026886b595be799ba4066f6d57f7
lucyia <lucia.koc@gmail.com>
2016-02-14 13:51:42 +0100

    Menu animate fixed

--- devel/js/functions.js
+++ devel/js/functions.js
@@ -1,15 +1,13 @@
 (function(window, document, undefined){
-	$(document).ready(function(){		
-		if ($("#index-navbar").prop("disabled")) {
-			$("#index-toggle").on("click", function(){
-				$("#index-navbar").fadeOut("slow").animate({"left": "-70%" }, {duration: "slow", queue: false}, function() {/*done*/});
-				$("#index-navbar").prop("disabled", true);
-			});
-		} else {
-			$("#index-toggle").on("click", function(){
-				$("#index-navbar").fadeIn("slow").animate({"left": "70%" }, {duration: "slow", queue: false}, function() {/*done*/});
-				$("#index-navbar").prop("disabled", false);
-			});
-		}
+	$(document).ready(function(){
+		$("#index-navbar").css("display", "none");
+
+		$("#index-toggle").on("click", function(){
+			if ($("#index-navbar").is(":visible")) {
+				$("#index-navbar").fadeToggle("slow").animate({"left": "70%" }, {duration: "slow", queue: false}, function() {/*done*/});
+			} else {				
+				$("#index-navbar").fadeToggle("slow").animate({"left": "0%" }, {duration: "slow", queue: false}, function() {/*done*/});				
+			}
+		});
 	});	
 })(this, document);
